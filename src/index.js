a07fd295533667b7b6ab3c75db918f2bd2c3a240
Rylan Gotto <rgotto2@gmail.com>
2023-09-01 16:37:20 -0600

    PAPIR-1 Create header and footer
    Header and footer components have been created and are now displaying

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-// import React from 'react';
-// import { ReactDOM } from 'react-dom';
-// import * as serviceWork from 'react-dom';
-// import './index.css'
-// import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
-// import App from './App';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
-// const routing = (
-//   <Router>
-//     <React.StrictMode>
-//       <Header />
-//       <Switch>
-//         <Route exact path="/" componenet={App} />
-//       </Switch>
-//       <Footer />
-//     </React.StrictMode>
-//   </Router>
-// );
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
+      <Footer />
+    </React.StrictMode>
+  </Router>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(routing);
